6bda0eda6bd414447bde8576f05e634c49a6dbc7
Richard <richard_friedrichsen@yahoo.de>
2023-11-09 11:41:11 +0200

    E-Mail login added successfully

--- A_Email/tasks.py
+++ A_Email/tasks.py
@@ -1,9 +1,44 @@
 from celery import shared_task
 import time
+from B_User.models import User
+
+import imaplib
+import email
 
 
 def retrivingEmails():
-    pass
+    # Getting User Credentials
+    server = User.objects.first().imap_server
+    password = User.objects.first().imap_password
+    email = User.objects.first().imap_email
+    port = User.objects.first().imap_port
+
+    # connection to the server
+    imap = imaplib.IMAP4_SSL(server, port)
+    imap.login(email, password)
+    imap.select("Inbox")
+
+    # Search Criteria
+    _, msgnums = imap.search(None, "ALL")
+
+    # Printing the number of E-Mails
+    num_emails = len(msgnums[0].split())
+    print(f"Number of emails in the inbox: {num_emails}")
+
+    # for mgsnum in msgnums[0].split():
+    #     _, data = imap.fetch(mgsnum, "(RFC822)")
+
+    #     message = email.message_from_bytes(data[0][1])
+    #     print(f"Message number: {mgsnum}")
+    #     print(f"From: {message.get('From')}")
+    #     print(f"To: {message.get('To')}")
+    #     print(f"Date: {message.get('Date')}")
+    #     print(f"Subject: {message.get('Subject')}")
+
+    #     for part in message.walk():
+    #         if part.get_content_type() == "text/plain":
+    #             print(part.as_string())
+    imap.close()
 
 
 @shared_task
--- A_Email/views.py
+++ A_Email/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .tasks import mock_action
+from .tasks import mock_action, retrivingEmails
 
 # Create your views here.
 
 
 def list_emails(request):
+    retrivingEmails()
     mock_action.delay()
     return render(request, "email.html")
